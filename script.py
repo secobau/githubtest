d7eb5e89e77e8d60b21e2c1a8be87ecf4d7e72a4
K3666 <68623052+K3666@users.noreply.github.com>
2020-10-25 22:46:00 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,3 @@
-Write a program that asks the user to enter their age in years as input (assume that the user enters a positive integer) and calculates and prints how old the user is in terms of days. Assume that there are 365 days in a year. For example: if the user enters
-
- 22
-then your program should print
-You are 8030 days old
-Pay attention to the spaces. In order to receive proper grade, your output should EXACTLY match the output shown in the sample solution (highlighted in yellow) including spaces and capitalization.
+age=int(input("write your age? "))
+days=age*365
+print("You are",days,"days old")
